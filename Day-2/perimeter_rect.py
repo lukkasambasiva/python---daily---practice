c1664d1efd99801be25dec058643fbffb634d3de
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-03 12:46:20 +0530

    Day 2 - Arithmetic Operators

---
+++ Day-2/perimeter_rect.py
@@ -0,0 +1,6 @@
+#perimeter of rectangle area
+num1=float(input('Enter length value :'))
+num2=float(input('Enter breadth value:'))
+per_rect =2*(num1+num2)
+print('Perimeter Of Rectangle :',per_rect)
+
