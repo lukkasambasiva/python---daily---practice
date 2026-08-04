1875308abe243a8847278202cd123500e7282c8d
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-04 11:36:08 +0530

    Day 2 - Python Operators
    Implement comparison operators and user input for two numbers.

---
+++ Day-2/comparsion.py
@@ -0,0 +1,19 @@
+'''comparsion operators'''
+
+num1= int(input('Enter num1 :'))
+num2= int(input('Enter num2 :'))
+
+not_equal = num1!=num2
+equal =num1==num2
+grat = num1>num2
+less =num1<num2
+grat_equal = num1>=num2
+less_equal =num1<=num2
+
+print('Not Equal :',not_equal)
+print('Equal :',equal)
+print('Grater :',grat)
+print('Less :',less)
+print('Grator or Equal :',grat_equal)
+print('Less or Equal :',less_equal)
+
