10a4b6ede7c3c54ab55ed5cf1241c96cb2a7d88e
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-04 11:24:34 +0530

    Day-3 Conditional Statements

---
+++ Day-3/nnested_if.py
@@ -0,0 +1,10 @@
+num=int(input('Enter a number :'))
+if num%2==0:
+   if num>=2 and num<=6:
+       print('balayya')
+   elif num>=7 and num<=15:
+       print('Pavan kalyan')
+   elif num>15:
+        print('prabas')
+else:
+    print('Chiranjeevi')
