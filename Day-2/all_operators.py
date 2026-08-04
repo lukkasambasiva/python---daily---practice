9c3c8738db622e0c3284600661cec1fd41777e65
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-04 12:13:35 +0530

    Day 2 - Python Operators
    Implement basic arithmetic operations and print results.

---
+++ Day-2/all_operators.py
@@ -0,0 +1,19 @@
+ #all arthmatic pronblems
+
+num1 = int(input('Enter the firts number :'))
+num2 = int(input('Enter the second Number :'))
+add_nums =num1+num2
+sub_nums =num1-num2
+multi_nums =num1*num2
+divi_nums =num1/num2
+floor_nums =num1//num2
+modu_nums =num1%num2
+power_nums =num1**num2
+
+print('Addition :',add_nums)
+print('subtraction :',sub_nums)
+print('Multipliclation :',multi_nums)
+print('Division :',divi_nums)
+print('Floor :',floor_nums)
+print('Modulus :',modu_nums)
+print('Power :',power_nums)
