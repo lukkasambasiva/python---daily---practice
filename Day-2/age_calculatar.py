6978691beaf07b151e732c189557e4c18cdcdcb5
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-04 10:51:14 +0530

    Day 2 - Arithmetic Operators
    Implement age calculator that computes age in years, months, and days.

---
+++ Day-2/age_calculatar.py
@@ -0,0 +1,19 @@
+#age calculator
+'''step1:remember to bith year
+step2:remember to current passing year
+step3:subrction current passing year- birth year
+step4:show the age'''
+
+
+
+birth_year =int(input("Enter the Birth Year :"))
+current_year= int(input("Enter The Current year :"))
+age = (current_year - birth_year)
+months= age*12
+days = age*365
+print("Your Age :",age)
+print("In months :",months)
+print("In Days :",days)
+
+
+
