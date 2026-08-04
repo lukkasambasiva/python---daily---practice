a6a27c3caac5d0c25efafd34701ac4be1f39a7f8
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-04 12:23:11 +0530

    Day-3 Conditional Statements

---
+++ Day-3/expriance_bonus.py
@@ -0,0 +1,10 @@
+#Exprinace bonus
+exp = int(input('Enter your Expriance :'))
+if exp<5:
+    print('5% Bonus')
+elif exp<=10:
+    print('10% Bonus')
+elif exp>10:
+    print('20% Bonus')
+else:
+    print('0% Bonus')
