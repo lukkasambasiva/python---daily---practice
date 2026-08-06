f32ce023a47390186dd0ed1cdc5d6ed435a3bb86
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-06 10:45:34 +0530

    Day-5 Loops

---
+++ Day-5/one-n numbers_divisibleBy-4.py
@@ -0,0 +1,26 @@
+#1 to n numbers and divisible by 4
+'''num=int(input('enter a number :'))
+i=1
+while i<=num:
+    if i%4==0:
+        print(i)
+    i=i+1
+#
+num=int(input('Enter a mumber :'))
+i=1
+s=0
+while i<=num:
+    if i%3==0 and i%2!=0:
+        s=s+1
+        print(i)
+
+    i=i+1'''
+#
+num=int(input('Enter a number :'))
+i=1
+while i<=num:
+    if i%5!=0:
+        print(i)
+        
+    
+    i=i+1
