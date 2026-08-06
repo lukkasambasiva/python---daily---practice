25aee379eeda7d9d9f855fd4063ec95166c7bf8d
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-06 11:01:44 +0530

    Day-5 Loops

---
+++ Day-5/usingNestedloop.py
@@ -0,0 +1,11 @@
+#print all prime numbers
+num=int(input('Enter  a number:'))
+i=1
+c=0
+while i<=num:
+    if num%i==0:
+        while j<50:
+            if 
+        c=c+1
+        print(c)
+    i=i+1
