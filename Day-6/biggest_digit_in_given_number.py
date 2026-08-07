b960475dc975674a73f7a76e2588abb6c1aec182
LUKKA SAMBA SIVA <lukkasambasiva07@gmail.com>
2026-08-07 10:56:43 +0530

    Day-6 Loops

---
+++ Day-6/biggest_digit_in_given_number.py
@@ -0,0 +1,9 @@
+#biggest digit in given number
+num=int(input('Enter a number:'))#153
+big=0
+while num>0:#153>0=T
+    d = num%10#153%10=3
+    if d>big:#5>30-t
+        big = d#5
+    num = num//10#153//10=15
+print(big) 
